a93fb947dae1911039ee1455fd05340d54abb78e
Gerth, Matthias <Matthias.Gerth@softwareag.com>
2026-03-02 16:52:45 +0100

    fix: Change pino import to default import syntax

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import * as pino from 'pino';
+import pino from 'pino';
 
 // ---------------------------------------------------------------------------
 // Log level
